a0fad4cfeaa48a7057344954363127c6c50c16cb
StesNiash <111519914+StesNiash@users.noreply.github.com>
2025-04-07 00:35:07 +0300

    Update main.py

--- tvDatafeed/main.py
+++ tvDatafeed/main.py
@@ -108,6 +108,7 @@ class TvDatafeed:
                 # Теперь используем cookies для получения данных
                 response = requests.get("https://www.tradingview.com", cookies=cookie_dict, headers=self.__signin_headers)
                 token = response.json()['user']['auth_token']
+                print(response.json())
             except Exception as e:
                 logger.error('error while signin')
                 token = None
