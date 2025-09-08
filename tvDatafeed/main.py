b41b111d9ca546ef47058718d7de14c70b71df1e
StesNiash (Laptop) <111519914+StesNiash@users.noreply.github.com>
2025-09-09 01:24:57 +0300

    Everything seems working
    tests are removed, everything seems just fine.

--- tvDatafeed/main.py
+++ tvDatafeed/main.py
@@ -205,32 +205,10 @@ class TvDatafeed:
                 token = None
                 
                 try:
-                    # Настройки для ускорения Chrome
-                    chrome_options = webdriver.ChromeOptions()
-                    chrome_options.add_argument('--no-sandbox')
-                    chrome_options.add_argument('--disable-dev-shm-usage')
-                    chrome_options.add_argument('--disable-gpu')
-                    chrome_options.add_argument('--disable-extensions')
-                    chrome_options.add_argument('--disable-plugins')
-                    chrome_options.add_argument('--disable-images')  # Не загружать изображения
-                    chrome_options.add_argument('--disable-web-security')
-                    chrome_options.add_argument('--aggressive-cache-discard')
-                    chrome_options.add_argument('--memory-pressure-off')
-                    chrome_options.add_argument('--disable-features=VizDisplayCompositor')
-                    chrome_options.add_argument('--disable-background-timer-throttling')
-                    chrome_options.add_argument('--disable-renderer-backgrounding')
-                    chrome_options.add_argument('--disable-backgrounding-occluded-windows')
-                    chrome_options.add_argument('--disable-ipc-flooding-protection')
-                    chrome_options.add_experimental_option('useAutomationExtension', False)
-                    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-                    
-                    print("Запуск оптимизированного браузера для решения капчи...")
-                    driver = webdriver.Chrome(options=chrome_options)
-                    driver.set_page_load_timeout(15)  # Таймаут загрузки страницы
-                    
-                    print("Загрузка страницы входа...")
+                    driver = webdriver.Chrome()
                     driver.get(self.__sign_in_url)
                     
+                    time.sleep(2)  
                     print("Пожалуйста, решите капчу и войдите в систему.")
     
                     try:
