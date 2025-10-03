5669d8b36e6250a8ccc31dd0375d00037671f251
kirtimiyani <94520445+kirtimiyani@users.noreply.github.com>
2025-10-03 10:53:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
+with open("README.md", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
@@ -28,3 +28,4 @@ setup(
         "requests"
     ],
 )
+
