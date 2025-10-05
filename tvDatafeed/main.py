b2f3f06dbb163cda42bcf310a03ad191b3db9f82
kirtimiyani <94520445+kirtimiyani@users.noreply.github.com>
2025-10-05 10:51:24 +0530

    Update main.py

--- tvDatafeed/main.py
+++ tvDatafeed/main.py
@@ -210,18 +210,18 @@ class TvDatafeed:
                 token = None
                 
                 try:
-                    if config('CHANNELS_CONFIG') == "REDIS":
-                    # for runnning loaclly
-                        driver = webdriver.Chrome()
+                    # if config('CHANNELS_CONFIG') == "REDIS":
+                    # # for runnning loaclly
+                    #     driver = webdriver.Chrome()
 
-                    else:                    
+                    # else:                    
                     # for running on server
-                        pc_ip = ProjectSettings.objects.get(key="pc_ip").value
-                        options = webdriver.ChromeOptions()
-                        driver = webdriver.Remote(
-                            command_executor="http://"+pc_ip+":4444/wd/hub",
-                            options=options
-                        )
+                    pc_ip = ProjectSettings.objects.get(key="pc_ip").value
+                    options = webdriver.ChromeOptions()
+                    driver = webdriver.Remote(
+                        command_executor="http://"+pc_ip+":4444/wd/hub",
+                        options=options
+                    )
                     
                     driver.get(self.__sign_in_url)
                     
